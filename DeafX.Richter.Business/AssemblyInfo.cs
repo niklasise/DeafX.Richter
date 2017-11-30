becc13d9b11dcf44149a4dc8e0312bf740896dbc
Kontor <niis2015@outlook.com>
2017-11-30 22:13:06 +0100

    Remove ignored files

--- DeafX.Richter.Business/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿// some .cs file included in your project
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("DeafX.Richter.Business.Test")]
--- DeafX.Richter.Business/Models/ZWay/AuthenticationParameters.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DeafX.Richter.Business.Models.ZWay
-{
-    public class AuthenticationParameters
-    {
-        public string login { get; set; }
-        public string password { get; set; }
-        public bool remeberme { get; set; }
-    }
-}
